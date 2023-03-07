1efc66c73002bc77ede4bea92548e2139c062f79
Fergal Moran <fergal.moran@gmail.com>
2023-03-07 21:11:03 +0000

    Tag docker image as latest

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0-alpha.13+Branch.develop.Sha.e1607ebd0de7cde2857560ce414d49d4deab5a3e")]
+[assembly: AssemblyInformationalVersion("0.2.0-alpha.14+Branch.develop.Sha.502dcef2326d8a65a4b6af7113f0c6568496c99e")]
